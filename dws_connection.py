e9e340230cf0452c5787a947a31df90ae7b5920e
Hikomorebi <1207769645@qq.com>
2024-11-21 21:13:44 +0800

    优化项目结构

--- dws_connection.py
+++ dws_connection.py
@@ -1,51 +1,28 @@
-#!/usr/bin/env python3
-# _*_ encoding=utf-8 _*_
 
+import time
 import psycopg2
-from psycopg2.extras import RealDictCursor
-
-def dws_connect():
-    # 创建连接
-    conn = psycopg2.connect(dbname="fdc_dc",
-                            user="dws_user_hwai",
-                            password="NewHope#1982@",
-                            host="124.70.57.67",
-                            port="8000")
-    conn.set_client_encoding('UTF8')
+def dws_connect_test(sql_query):
+    connection = psycopg2.connect(
+        dbname="fdc_dc",
+        user="dws_user_hwai",
+        password="NewHope#1982@",
+        host="124.70.57.67",
+        port="8000",
+    )
+    connection.set_client_encoding('UTF8')
     print("连接成功")
 
-    # 使用默认游标执行SQL，查询结果是元祖
-    query = "SELECT roomcode, roomname, SUM(sub_units) AS total_sub_units FROM fdc_dws.dws_proj_room_totalsale_a_min GROUP BY roomcode, roomname ORDER BY total_sub_units DESC LIMIT 1;"
-    
-    print("Start to execute: \"%s\"" % query)
-    cursor = conn.cursor()
-    cursor.execute(query)
-    rows = cursor.fetchall()
+    try:
+        with connection.cursor() as cursor:
+            start_time = time.time()
+            cursor.execute(sql_query)
 
-    print("Output:")
-    print(', '.join([field.name for field in cursor.description]))
-    for row in rows:
-        print(', '.join([str(e) for e in row]))
-    cursor.close()
-
-    # 使用RealDictCursor游标执行SQL，查询结果是字典
-    '''query = "select * from student"
-
-    print("Start to execute: \"%s\"" % query)
-    cursor = conn.cursor(cursor_factory = RealDictCursor)
-    cursor.execute(query)
-    records = cursor.fetchall()
-    field_names = [field.name for field in cursor.description]
-    cursor.close()
-
-    print("Output:")
-    row = 0
-    for record in records:
-        row += 1
-        print("-[ RECORD %d ]-" % row)
-        for name in field_names:
-            print("%-5s | %-5s" % (name, record[name]))
-    print("(%d %s)" % (row, "rows" if row > 1 else "row"))
-
-    conn.close()'''
-dws_connect()
+            results = cursor.fetchall()
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            print(f"查询耗时{elapsed_time}秒")
+            print(results)
+    except Exception as e:
+        print(str(e))
+    finally:
+        connection.close()
--- utils.py
+++ utils.py
@@ -566,32 +566,6 @@ def dws_connect(sql_query,key_fields=None,display_type="response_bar_chart"):
 
     return dws_connect_dict
 
-def dws_connect_test(sql_query):
-    connection = psycopg2.connect(
-        dbname="fdc_dc",
-        user="dws_user_hwai",
-        password="NewHope#1982@",
-        host="124.70.57.67",
-        port="8000",
-    )
-    connection.set_client_encoding('UTF8')
-    print("连接成功")
-
-    try:
-        with connection.cursor() as cursor:
-            start_time = time.time()
-            cursor.execute(sql_query)
-
-            results = cursor.fetchall()
-            end_time = time.time()
-            elapsed_time = end_time - start_time
-            print(f"查询耗时{elapsed_time}秒")
-            print(results)
-    except Exception as e:
-        print(str(e))
-    finally:
-        connection.close()
-
 def extract_json_fields(input_string):
     # Use regex to find potential JSON parts in the input string
     # json_matches = re.findall(r"{.*?}", input_string, re.DOTALL)
